8a8284cf5c75c7317f9450f54c5303b77ca0c3c1
ccnuyan <ccnuyan@gmail.com>
2017-12-26 15:46:59 +0800

    oauth services to be done

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
 import '../globals';
-import { pg } from '../database/connector';
 import config from '../config';
 import routes from '../src';
 import tokenAuth from './middleware/tokenAuth';
@@ -20,54 +19,38 @@ const app = express();
 const PORT = process.env.PORT || config.port;
 global.report();
 
-try {
-  (async () => {
-    const pgPool = await pg.connect()
-      .catch(err => global.printError(err, __dirname));
+// express middleware
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
-    console.log(`database ${config.pg.host + ':' + config.pg.port} connected`); // eslint-disable-line
+app.use('/static/', express.static(path.join(__dirname, '../build/')));
+app.use('/static/', express.static(path.join(__dirname, '../public/')));
 
-    // express middleware
-    app.use(compression());
-    app.use(cookieParser());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-      extended: true,
-    }));
+// custom middlewares
+app.use(utilities.ajaxDetector);
+app.use(crossDomain);
 
-    app.use('/static/', express.static(path.join(__dirname, '../build/')));
-    app.use('/static/', express.static(path.join(__dirname, '../public/')));
-
-    app.use(crossDomain);
-
-    app.use((req, res, next) => {
-      req.context = { pgPool };
-      next();
-    });
-
-    if (config.mode === 'development') {
-      app.use(delay(100, 300));
-      app.use(morgan('tiny'));
-    }
-
-    // ajaxDetector
-    app.use(utilities.ajaxDetector);
-
-    // auth
-    app.use(tokenAuth);
-
-    routes(app);
-
-    app.listen(PORT, (err) => {
-      if (err) {
-        printError(err, __filename);
-      } else {
-        printMessage(`syncollege is listening on port ${PORT}`, __filename);
-      }
-    });
-  })();
-} catch (err) {
-  printError(err, __filename);
+if (config.mode === 'development') {
+  app.use(delay(100, 300));
+  app.use(morgan('tiny'));
 }
-/* eslint-disable no-console */
+
+// auth
+app.use(tokenAuth);
+
+routes(app);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    printError(err, __filename);
+  } else {
+    printMessage(`${config.title} is listening on port ${PORT}`, __filename);
+  }
+});
+
+export default app;
 
