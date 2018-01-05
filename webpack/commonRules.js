96df640f2ef7c5b9000535b3f7ae4dd11607e3e3
ccnuyan <ccnuyan@gmail.com>
2018-01-05 12:02:51 +0800

    remove makedownloader

--- webpack/commonRules.js
+++ webpack/commonRules.js
@@ -76,27 +76,28 @@ module.exports = [{
       },
     ],
   }),
-}, {
-  test: /\.md$/,
-  use: [
-    {
-      loader: 'html-loader',
-    },
-    {
-      loader: 'markdown-loader',
-    },
-  ],
-  // }, {
-  //   test: /\.woff(2)?(\?[a-z0-9]+)?$/,
-  //   use: {
-  //     loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-  //   },
-  // }, {
-  //   test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
-  //   use: {
-  //     loader: 'file-loader',
-  //   },
 },
+// {
+//   test: /\.md$/,
+//   use: [
+//     {
+//       loader: 'html-loader',
+//     },
+//     {
+//       loader: 'markdown-loader',
+//     },
+//   ],
+//   // }, {
+//   //   test: /\.woff(2)?(\?[a-z0-9]+)?$/,
+//   //   use: {
+//   //     loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+//   //   },
+//   // }, {
+//   //   test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
+//   //   use: {
+//   //     loader: 'file-loader',
+//   //   },
+// },
 {
   test: /\.html$/,
   use: [{
