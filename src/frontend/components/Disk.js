eb3ac8ef9d7d4e1d2cdceec8e04280676f88e22b
ccnuyan <ccnuyan@gmail.com>
2018-01-05 13:55:33 +0800

    animation loader

--- src/frontend/components/Disk.js
+++ src/frontend/components/Disk.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Motion, spring } from 'react-motion';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import FileList from './FileList';
@@ -6,18 +7,26 @@ import Filter from './Filter';
 import Header from './Header';
 
 class Disk extends Component {
+  componentDidMount() {
+    $('#cssload-thecube').css({ display: 'none' });
+  }
+
   render = () => {
     return (
-      <div className="starc-disk">
-        <Header/>
-        <div className="ui container">
-          <div className="ui basic segment">
-            <Filter/>
-            <div className="ui hidden divider"></div>
-            <FileList />
+      <Motion defaultStyle={ { opacity: 0 } } style={ { opacity: spring(1) } }>
+        {
+          style => <div className="starc-disk" style={ style }>
+            <Header/>
+            <div className="ui container">
+              <div className="ui basic segment">
+                <Filter/>
+                <div className="ui hidden divider"></div>
+                <FileList />
+              </div>
+            </div>
           </div>
-        </div>
-      </div >
+        }
+      </Motion>
     );
   }
 }
